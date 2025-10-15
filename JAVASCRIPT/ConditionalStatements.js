94666c4ecd646755c28d05d3d5d976f1c08d887c
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-10-15 08:05:16 +0530

    Added JavaScript File

---
+++ JAVASCRIPT/ConditionalStatements.js
@@ -0,0 +1,17 @@
+console.log("IF STATEMENT");
+let mode="light";
+let color;
+if(mode==="dark"){
+    color="black";
+}
+if(mode!=="dark"){
+    color="white";
+}
+console.log("color is: ",color)
+console.log("IF-ELSE STATEMENT");
+let age=17;
+if(age>=18){
+    console.log("You Can Vote.");
+}else{
+    console.log("You Cannot Vote.")
+}
---
+++ JAVASCRIPT/welcome.js
@@ -0,0 +1,33 @@
+let fullName='Avadhut';
+console.log(fullName);
+
+let age=25;
+console.log(age)
+
+let student={
+    fullName:'Ram ',
+    age:25,
+    cgpa:8.8,
+    isPass:true
+};
+
+console.log(student.fullName);
+console.log(student["age"]);
+console.log(student.cgpa);
+
+// CONSTANT OBJECT EXAMPLE
+const product={
+    productTitle:"Ball Pen",
+    price:500,
+    rating:4,
+    isOffer:true,
+    discount:5
+};
+
+const profile={
+    userName:"Avadhut",
+    posts:125,
+    followers:"145k",
+    following:5,
+    about:"Software Developer"
+};
