f12d5c45ac553a407dfc3ac27e9eb7b5b6628e5b
AvadhutJoshi012 <avadhutjoshi012@gmail.com>
2025-10-19 09:26:00 +0530

    JS Operators and simple Problem Added

---
+++ JAVASCRIPT/PracticeQuestion01.js
@@ -0,0 +1,13 @@
+// alert("hello");  //used to show the one time popup message
+// prompt("avadhut");  // used to get input.
+
+// let name = prompt("Enter Your Name");
+// console.log(name);
+// document.write(name);
+
+let number = prompt("Enter Number to Check: ");
+if (number % 5 == 0) {
+  console.log("Multiple of 5.");
+} else {
+  console.log("Not multiple of 5.");
+}
